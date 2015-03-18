44f73602eb0d737b131827b8023b8689f6c14ad0
Michael V. <michaelvrld@gmail.com>
2015-03-18 17:53:20 +0100

    Added dKills, cCraft, cTrees and cItems

--- sql_dump.sql
+++ sql_dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 15, 2015 at 06:00 PM
+-- Generation Time: Mar 18, 2015 at 12:51 PM
 -- Server version: 5.0.95
 -- PHP Version: 5.3.3
 
@@ -13,37 +13,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `TLOD`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `inv_data`
---
-
-CREATE TABLE IF NOT EXISTS `inv_data` (
-  `iID` int(11) NOT NULL auto_increment,
-  `Name` varchar(24) NOT NULL,
-  `ItemModel` int(11) NOT NULL,
-  `ItemAmount` int(11) NOT NULL,
-  PRIMARY KEY  (`iID`),
-  KEY `Name` (`Name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=447 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tree_data`
---
-
-CREATE TABLE IF NOT EXISTS `tree_data` (
-  `tID` int(11) NOT NULL auto_increment,
-  `ModelID` int(11) NOT NULL,
-  `Health` float NOT NULL default '100',
-  `Pos_X` float NOT NULL,
-  `Pos_Y` float NOT NULL,
-  `Pos_Z` float NOT NULL,
-  PRIMARY KEY  (`tID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=451 ;
 
 -- --------------------------------------------------------
 
@@ -63,6 +32,10 @@ CREATE TABLE IF NOT EXISTS `user_data` (
   `Level` smallint(6) NOT NULL default '0',
   `hKills` int(11) NOT NULL default '0',
   `zKills` int(11) NOT NULL default '0',
+  `dKills` int(11) NOT NULL default '0',
+  `cItems` int(11) NOT NULL default '0',
+  `cCraft` int(11) NOT NULL default '0',
+  `cTrees` int(11) NOT NULL default '0',
   `InvSize` int(11) NOT NULL default '5',
   `Pos_X` float NOT NULL default '0',
   `Pos_Y` float NOT NULL default '0',
@@ -72,19 +45,3 @@ CREATE TABLE IF NOT EXISTS `user_data` (
   `Last_Login` timestamp NOT NULL default '0000-00-00 00:00:00' on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vehicle_data`
---
-
-CREATE TABLE IF NOT EXISTS `vehicle_data` (
-  `vID` int(11) NOT NULL auto_increment,
-  `Model` int(11) NOT NULL,
-  `Pos_X` float NOT NULL,
-  `Pos_Y` float NOT NULL,
-  `Pos_Z` float NOT NULL,
-  `Rot` float NOT NULL,
-  PRIMARY KEY  (`vID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
