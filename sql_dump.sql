6f33a78fef567643fcb6a356decca29bdd453656
Michael V. <michaelvrld@gmail.com>
2015-03-29 18:16:26 +0200

    I noticed i forgot 2 tables

--- sql_dump.sql
+++ sql_dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 18, 2015 at 12:51 PM
+-- Generation Time: Mar 29, 2015 at 12:15 PM
 -- Server version: 5.0.95
 -- PHP Version: 5.3.3
 
@@ -13,6 +13,37 @@ SET time_zone = "+00:00";
 --
 -- Database: `TLOD`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inv_data`
+--
+
+CREATE TABLE IF NOT EXISTS `inv_data` (
+  `iID` int(11) NOT NULL auto_increment,
+  `Name` varchar(24) NOT NULL,
+  `ItemModel` int(11) NOT NULL,
+  `ItemAmount` int(11) NOT NULL,
+  PRIMARY KEY  (`iID`),
+  KEY `Name` (`Name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=447 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tree_data`
+--
+
+CREATE TABLE IF NOT EXISTS `tree_data` (
+  `tID` int(11) NOT NULL auto_increment,
+  `ModelID` int(11) NOT NULL,
+  `Health` float NOT NULL default '100',
+  `Pos_X` float NOT NULL,
+  `Pos_Y` float NOT NULL,
+  `Pos_Z` float NOT NULL,
+  PRIMARY KEY  (`tID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=451 ;
 
 -- --------------------------------------------------------
 
@@ -45,3 +76,19 @@ CREATE TABLE IF NOT EXISTS `user_data` (
   `Last_Login` timestamp NOT NULL default '0000-00-00 00:00:00' on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle_data`
+--
+
+CREATE TABLE IF NOT EXISTS `vehicle_data` (
+  `vID` int(11) NOT NULL auto_increment,
+  `Model` int(11) NOT NULL,
+  `Pos_X` float NOT NULL,
+  `Pos_Y` float NOT NULL,
+  `Pos_Z` float NOT NULL,
+  `Rot` float NOT NULL,
+  PRIMARY KEY  (`vID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
